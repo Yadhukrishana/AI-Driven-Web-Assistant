9ae5c98305a3dd1256aaf0f3e985438e4c4e8c4a
unknown <yedhu8566@gmail.com>
2025-05-17 18:39:51 +0530

    project implementation

---
+++ src/main/java/com/yadhu/ai_driven_web_assistant/AiDrivenWebAssistantApplication.java
@@ -0,0 +1,13 @@
+package com.yadhu.ai_driven_web_assistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiDrivenWebAssistantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiDrivenWebAssistantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yadhu/ai_driven_web_assistant/AiDrivenWebAssistantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yadhu.ai_driven_web_assistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiDrivenWebAssistantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
